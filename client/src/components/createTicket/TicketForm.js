99c1e4399f4bfb6d64c786f5e3470875e5157428
Dehn Hunsworth <dhunsworth@yahoo.com>
2019-11-19 08:10:27 -0700

    implementing google address search, form page style

--- client/src/components/createTicket/TicketForm.js
+++ client/src/components/createTicket/TicketForm.js
@@ -6,6 +6,13 @@ import { connect } from "react-redux";
 import { addTicket } from "../../store/actions/tickets";
 import tickets from "../../store/reducers/tickets";
 
+import PlacesAutocomplete from "react-places-autocomplete";
+import {
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng
+} from "react-places-autocomplete";
+
 class TicketForm extends React.Component {
   state = {
     name: "",
@@ -13,7 +20,8 @@ class TicketForm extends React.Component {
     city: "",
     zip: 0,
     dateRequested: new Date(),
-    serviceRequested: ""
+    serviceRequested: "",
+    autoAddress: ""
   };
 
   rerouteToTickets = () => {
@@ -22,6 +30,16 @@ class TicketForm extends React.Component {
 
   onDateChange = date => this.setState({ date });
 
+  handleChange = address => {
+    this.setState({ autoAddress: address });
+  };
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log("Success", latLng))
+      .catch(error => console.error("Error", error));
+  };
   change = e => {
     this.setState({
       [e.target.name]: e.target.value
@@ -79,34 +97,82 @@ class TicketForm extends React.Component {
       <div className="ticket-form-wrapper">
         <h2>Create Ticket Form</h2>
         <form action="post" onSubmit={e => this.onSubmit(e)}>
-          <div className="left-column">
-            <label htmlFor="name">Customer Name</label>
-            <input type="text" name="name" onChange={e => this.change(e)} />
-            <label htmlFor="address">Address</label>
+          <label htmlFor="name">Customer Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="enter name ..."
+            onChange={e => this.change(e)}
+          />
+          <label>Address</label>
+          <PlacesAutocomplete
+            value={this.state.autoAddress}
+            onChange={this.handleChange}
+            onSelect={this.handleSelect}
+          >
+            {({
+              getInputProps,
+              suggestions,
+              getSuggestionItemProps,
+              loading
+            }) => (
+              <div className="autocomplete-address">
+                <input
+                  {...getInputProps({
+                    placeholder: "Search Places ...",
+                    className: "location-search-input"
+                  })}
+                />
+                <div className="autocomplete-dropdown-container">
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map(suggestion => {
+                    const className = suggestion.active
+                      ? "suggestion-item--active"
+                      : "suggestion-item";
+                    // inline style for demonstration purpose
+                    const style = suggestion.active
+                      ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                      : { backgroundColor: "#ffffff", cursor: "pointer" };
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                          style
+                        })}
+                      >
+                        <span>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
+          {/* <label htmlFor="address">Address</label>
             <input type="text" name="address" onChange={e => this.change(e)} />
             <label htmlFor="city">City</label>
             <input type="text" name="city" onChange={e => this.change(e)} />
             <label htmlFor="zip">Zipcode</label>
-            <input type="text" name="zip" onChange={e => this.change(e)} />
-          </div>
-          <div className="right-column">
-            <label htmlFor="date">Date requested</label>
-            <DateTimePicker
-              name="dateRequested"
-              onChange={this.onDateChange}
-              value={this.state.dateRequested}
-              hourAriaLabel="true"
-              dayAriaLable="true"
-            />
-            <label htmlFor="service">Service Requested</label>
-            <textarea
-              name="serviceRequested"
-              id="service"
-              cols="30"
-              rows="10"
-              onChange={e => this.change(e)}
-            ></textarea>
-          </div>
+            <input type="text" name="zip" onChange={e => this.change(e)} /> 
+
+          <label htmlFor="date">Date requested</label>
+           <DateTimePicker
+            name="dateRequested"
+            onChange={this.onDateChange}
+            value={this.state.dateRequested}
+            hourAriaLabel="true"
+            dayAriaLable="true"
+          /> */}
+          <label htmlFor="service">Service Requested</label>
+          <textarea
+            name="serviceRequested"
+            placeholder="describe service needed ..."
+            id="service"
+            cols="30"
+            rows="10"
+            onChange={e => this.change(e)}
+          ></textarea>
+
           <div className="button-area">
             <button type="submit" id="submit-button">
               Submit Ticket
